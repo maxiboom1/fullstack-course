e3016f59c222a9f78fcd678fe0521b87960cf75a
Alex Samih-zade <galutziogen@gmail.com>
2022-11-18 00:16:29 +0200

    ex-1

--- jquerry-1/hw/01/assets/index.js
+++ jquerry-1/hw/01/assets/index.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
     $('.btn1, .btn2').click(handleClick);  
     
     function handleClick(event){
-        const index = event.currentTarget.className.slice(-1); // fetchs the classname (btn1 or btn2 and sclices  last char (1 or 2))
+        const index = event.currentTarget.className.slice(-1); // fetchs the classname (btn1 or btn2 and slices last char (1 or 2))
         $('.target' + index).html($('.input' + index).val());
         $('.input' + index).hide();
     }
